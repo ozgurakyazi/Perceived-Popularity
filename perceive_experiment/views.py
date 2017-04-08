c203e0c507435a69ff9b575f238d747b09019e6b
ozgurakyazi <ozgur.akyazi@boun.edu.tr>
2017-04-08 14:12:05 +0300

    non logged in users are not able to vote the content

--- perceive_experiment/views.py
+++ perceive_experiment/views.py
@@ -1,25 +1,28 @@
 from django.shortcuts import render
-
+from django.shortcuts import redirect
 # Create your views here.
 def vote_page(request):
-    return render(request, "vote_page.html",
-        { "images_info":[
-            {"image_ids":[
-                1,2,3,4,5
-            ]
-            },
-            {"like_counts":[
-                1,2,3,2,1
-            ]
-            },
-            {"dislike_counts":[
-                1,0,3,2,1
-            ]
-            },
-            {"like_status":[
-                -1,0,0,1,1
-            ]
+    if request.user.is_authenticated():
+        return render(request, "vote_page.html",
+            { "images_info":[
+                {"image_ids":[
+                    1,2,3,4,5
+                ]
+                },
+                {"like_counts":[
+                    1,2,3,2,1
+                ]
+                },
+                {"dislike_counts":[
+                    1,0,3,2,1
+                ]
+                },
+                {"like_status":[
+                    -1,0,0,1,1
+                ]
+                }
+             ]
             }
-         ]
-        }
-    )
+        )
+    else:
+        return redirect("account_login")
